bef99a042b5beabf2bc227476159db6e0b078d30
Alexander Waya Nguku <alex.waya@gmail.com>
2025-02-27 21:14:28 +0300

    setup user profile

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
 from .forms import CustomUserChangeForm, CustomUserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Profile
+
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
@@ -54,3 +59,64 @@ class CustomUserAdmin(UserAdmin):
     )
     search_fields = ("email",)
     ordering = ("email",)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = "Profile"
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    model = CustomUser
+
+    list_display = (
+        "username",
+        "email",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "last_login",
+    )
+    list_filter = ("is_active", "is_staff", "is_superuser")
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Dates", {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "username",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+    inlines = (ProfileInline,)
+
+
+# @admin.register(CustomUser)
+admin.site.register(Profile)
--- users/models.py
+++ users/models.py
@@ -5,6 +5,15 @@ from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
 
+
+import os
+
+from django.conf import settings
+from django.db import models
+from django.template.defaultfilters import slugify
+
+
+
 class CustomUser(AbstractUser):
     email = models.EmailField(_("email address"), unique=True)
 
@@ -15,3 +24,21 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+        
+def get_image_filename(instance, filename):
+    name = instance.product.name
+    slug = slugify(name)
+    return f"products/{slug}-{filename}"
+
+class Profile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to=get_image_filename, blank=True)
+    bio = models.CharField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.user.email
+
+    @property
+    def filename(self):
+        return os.path.basename(self.image.name)
